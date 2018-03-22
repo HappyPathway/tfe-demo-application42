d171c92bfbcda95e15a49cacc7b073cf37c0e51a
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-03-21 23:32:33 -0700

    master: adding in gcp instance module

--- main.tf
+++ main.tf
@@ -45,3 +45,48 @@ module "ec2_instance" {
   ttl           = "${var.ec2_instance_ttl}"
   #description  = "${var.ec2_instance_description}"
 }
+
+# GCP instance information
+variable "compute_instance_gcp_credentials" {
+  description = "GCP credentials needed by google provider"
+}
+
+variable "compute_instance_gcp_project" {
+  description = "GCP project name"
+}
+
+variable "compute_instance_gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default = "us-east1"
+}
+
+variable "compute_instance_gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default = "us-east1-b"
+}
+
+variable "compute_instance_image" {
+  description = "image to build instance from"
+  default = "debian-cloud/debian-8"
+}
+variable "compute_instance_instance_name" {
+  description = "GCP instance name"
+  default = "app42-instance"
+}
+variable "compute_instance_machine_type" {
+  description = "GCP machine type"
+  default = "n1-standard-1"
+}
+
+module "compute_instance" {
+  source  = "app.terraform.io/jake-lundberg/compute-instance/gpc"
+  version = "1.0.0"
+
+  gcp_credentials = "${var.compute_instance_gcp_credentials}"
+  gcp_project     = "${var.compute_instance_gcp_project}"
+  gcp_region      = "${var.compute_instance_gcp_region}"
+  gcp_zone        = "${var.compute_instance_gcp_zone}"
+  image           = "${var.compute_instance_image}"
+  instance_name   = "${var.compute_instance_instance_name}"
+  machine_type    = "${var.compute_instance_machine_type}"
+}
